2622aa6aad963f800bce3b952a3e08582a1ab383
Atai824 <a.toktosunov.8207@westcliff.edu>
2025-06-08 23:45:44 -0700

    Week 4 day 3 homework done

--- contact.js
+++ contact.js
@@ -1,1 +1,17 @@
-const select = document
+var contactSelect = document.getElementById("contactMethod");
+var emailField = document.getElementById("emailField");
+var phoneField = document.getElementById("phoneField");
+
+contactSelect.addEventListener("change", function () {
+    var selectedValue = contactSelect.value;
+    if(selectedValue === "email") {
+        emailField.style.display = "block";
+        phoneField.style.display = "none";
+    } else if (selectedValue === "phone") {
+        phoneField.style.display = "block";
+        emailField.style.display = "none";
+    } else {
+        phoneField.style.display = "none";
+        emailField.style.display = "none";
+    }
+});
